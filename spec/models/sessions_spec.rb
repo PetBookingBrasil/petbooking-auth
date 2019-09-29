c0be52f4e8263f7bde2df78711889f626dc02052
Lucas Pesqueira <lucas@petbooking.com.br>
2019-09-29 19:17:33 -0300

    Add tests to rspec

--- spec/models/sessions_spec.rb
+++ spec/models/sessions_spec.rb
@@ -5,4 +5,9 @@ describe Session, type: :model do
   describe 'relations' do
     it { is_expected.to belong_to(:user) }
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:token) }
+    it { is_expected.to validate_presence_of(:device) }
+  end
 end

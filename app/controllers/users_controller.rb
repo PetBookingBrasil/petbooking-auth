4919301611af5ec8b56de237e6f15598ed577119
Lucas Matheus <lucas.passos@petbooking.com.br>
2019-10-02 21:23:37 -0300

    Controller changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
       render json: @user
@@ -33,19 +32,18 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
   def destroy
     @user.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
+
     def user_params
-      params.require(:user).permit(:uuid, :birthday, :current_sign_in_at, :sign_in_count, :gender, :avatar, :current_sign_in_ip, :email, :last_sign_in_ip, :phone, :name, :nickname, :reset_password_token, :encrypted_password, :reset_password_sent_at, :remember_created_at, :last_sign_in_at, :bitmask, :http_user_agent, :avatar_processing, :cpf, :city, :state, :search_range, :zipcode, :street, :street_number, :password_token_expires_at, :neighborhood, :skype, :complement, :source, :validation_code, :created_by_business, :landline, :application_source)
+      params.require(:user).permit(:birthday, :email, :phone,
+        :name, :application_source)
     end
 end
